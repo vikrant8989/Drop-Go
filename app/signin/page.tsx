c2b24e5f1cbefe56bfd7d6986968291539eae844
vikrant <vikrantzone123@gmail.com>
2025-02-18 21:11:39 +0530

    signin fixes

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -153,7 +153,7 @@ function SignInForm() {
 
   return (
     <div className="container flex items-center justify-center min-h-screen">
-      <Card className="w-[50%] relative">
+      <Card className="md:w-[50%] w-full relative">
         <CardHeader>
           <CardTitle>Sign In or Sign Up</CardTitle>
           <CardDescription>
@@ -195,7 +195,7 @@ function SignInForm() {
                     Signing in with with email
                   </Button>
                 ) : (
-                  <Button className="w-[30%] mt-6" type="submit">
+                  <Button className="md:w-[30%] w-full mt-6" type="submit">
                     Sign in with email
                   </Button>
                 )}
